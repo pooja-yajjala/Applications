3767441a5a852fab79c6acc8ed1c1d05e5796e98
pooja-yajjala <83748162+pooja-yajjala@users.noreply.github.com>
2021-05-13 15:31:07 +0530

    Add files via upload

---
+++ SpringMVCCRUD-12thmay/src/main/java/com/crudbeans/Emp.java
@@ -0,0 +1,48 @@
+package com.crudbeans;  
+  
+public class Emp {  
+private int id;  
+private String name;  
+private float salary;  
+private String designation; 
+private String phoneno;
+private String email;
+  
+public String getPhoneno() {
+	return phoneno;
+}
+public void setPhoneno(String phoneno) {
+	this.phoneno = phoneno;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public int getId() {  
+    return id;  
+}  
+public void setId(int id) {  
+    this.id = id;  
+}  
+public String getName() {  
+    return name;  
+}  
+public void setName(String name) {  
+    this.name = name;  
+}  
+public float getSalary() {  
+    return salary;  
+}  
+public void setSalary(float salary) {  
+    this.salary = salary;  
+}  
+public String getDesignation() {  
+    return designation;  
+}  
+public void setDesignation(String designation) {  
+    this.designation = designation;  
+}  
+  
+}  
---
+++ SpringMVCCRUD-12thmay/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<body>
+<div>
+<p>
+About HRMS
+
+</p>
+</div>
+To Add Employees Click Below:<br>
+<br>
+<br>
+<a href="employeeform">Add Employee</a><br>
+<a href="view">View Employees</a>
+</body>
+</html>
